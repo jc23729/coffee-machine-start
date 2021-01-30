928a38ad884fe9f8cf4721097e819d2f44b5fd13
Juan Cruz <jc23729@gmail.com>
2021-01-30 19:58:23 +0000

    update

--- main.py
+++ main.py
@@ -67,4 +67,16 @@ def is_transaction_successful(money_received, drink_cost):
 
 while is_on:
     choice = input("​What would you like? (espresso/latte/cappuccino): ")
-    
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
